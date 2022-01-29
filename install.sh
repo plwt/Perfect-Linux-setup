703e20e0eb026bfce1c75bfe1c99cec7697d2a1d
Paul Wright <31939113+plwt@users.noreply.github.com>
2022-01-29 18:55:39 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+sudo apt-get install git blueman -yy
+
+sudo apt install snapd core
+
+wget 'https://download.mozilla.org/?product=firefox-nightly-latest-ssl&os=linux64&lang=en-GB' -O firefox-nightly.tar.bz2
+
+git clone https://github.com/plwt/Nightly.desktop.git
+
+git clone https://github.com/plwt/Thunderbird-Mail.desktop.git
+
+sudo ln -s /etc/profile.d/apps-bin-path.sh /etc/X11/Xsession.d/99snap
+
+sudo snap install code --classic
+
+sudo snap install josm zoom-client jupyter gimp
